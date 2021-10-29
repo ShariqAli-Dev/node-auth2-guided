37f074a496bffb13210c41170ba8b4a3547ebbd2
Dave Kidd <dave@davekidd.net>
2021-10-28 18:27:38 -0600

    wip

--- api/auth/restricted-middleware.js
+++ api/auth/restricted-middleware.js
@@ -1,4 +1,9 @@
 module.exports = (req, res, next) => {
-  // add code here to verify users are logged in
-  next();
+  const token = req.headers.authorization
+
+  if(!token){
+    res.status(401).json("Need a token!!!")
+  }else{
+    //check it out
+  }
 };
